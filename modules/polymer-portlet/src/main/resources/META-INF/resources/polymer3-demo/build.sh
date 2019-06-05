cde6ad87d6eeceaca11ec4b59a91f203da8c5e47
Sasha Firsov <sfirsov@simulationworks.com>
2019-06-04 22:00:39 -0700

    Support for multiple bundles and debug

--- modules/polymer-portlet/src/main/resources/META-INF/resources/polymer3-demo/build.sh
+++ modules/polymer-portlet/src/main/resources/META-INF/resources/polymer3-demo/build.sh
@@ -1,14 +1,17 @@
 polymer build
-
+replacef(){
+#    replace-in-file $1 $2 $3
+    replace-in-file $1 $2 $3 --verbose --isRegex
+}
 applyRootPath(){
     echo "$1"
     rootPath="/o/polymer-portlet/polymer3-demo/build/$1/"
     indexHtml="build/$1/index.html"
-    replace-in-file "/\<base(.*?)\>/g" " "                                  $indexHtml --verbose --isRegex
-    replace-in-file "/rootPath:(.*?)[\'\"]/[\'\"]/g" "rootPath: '${rootPath}'"          $indexHtml --verbose --isRegex
-    replace-in-file "/[\'\"]service-worker.js[\'\"]/g" "'${rootPath}service-worker.js'" $indexHtml --verbose --isRegex
-    replace-in-file "/src\=\"\.\//g" "src=\"${rootPath}"                                $indexHtml --verbose --isRegex
-    replace-in-file "/define\(\[\'\.\//g" "define(['${rootPath}"                        $indexHtml --verbose --isRegex
+    replacef "/\<base(.*?)\>/g" " "                                              $indexHtml
+    replacef "/rootPath:(.*?)[\'\"]/[\'\"]/g" "rootPath: '${rootPath}'"          $indexHtml
+    replacef "/[\'\"]service-worker.js[\'\"]/g" "'${rootPath}service-worker.js'" $indexHtml
+    replacef "/src\=\"\.\//g" "src=\"${rootPath}"                                $indexHtml
+    replacef "/define\(\[\'\.\//g" "define(['${rootPath}"                        $indexHtml
 }
 
 for D in ./build/*; do
