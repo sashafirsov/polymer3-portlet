c67a871ff166ffc9517f44d0ee92d2dabccdfb1a
suns <suns@firsov.net>
2020-02-05 23:44:12 -0800

    reading packages

--- modules/polymer-portlet/src/main/resources/META-INF/resources/polymer3-demo/build-dist.js
+++ modules/polymer-portlet/src/main/resources/META-INF/resources/polymer3-demo/build-dist.js
@@ -1,15 +1,78 @@
-const readPkg = require('read-pkg');
-const { walk } = require('estree-walker');
-const MagicString = require('magic-string');
-const fs = require('fs');
-const { lstatSync } = require('fs')
-const glob = require("glob");
-const isDirectory = source => { try{ return lstatSync(source).isDirectory() }catch(_){} }
+/**
+ * process build/ folder content using importmap.js
+ * output is generated to dist/ folder
+ *
+ * @type {{(options?: readPkg.NormalizeOptions): Promise<readPkg.NormalizedPackageJson>; (options: readPkg.Options): Promise<readPkg.PackageJson>; sync: {(options?: readPkg.NormalizeOptions): readPkg.NormalizedPackageJson; (options: readPkg.Options): readPkg.PackageJson}} | readPkg}
+ */
+import importmap from "./importmap.js";
+import readPkg from 'read-pkg';
+import  walk  from 'estree-walker';
+import MagicString from 'magic-string';
+import fs from 'fs';
+import { lstatSync } from 'fs';
+import glob from "glob";
+const isDirectory = source => { try{ return lstatSync(source).isDirectory() }catch(_){} };
 
 const readJson = fName => JSON.parse( fs.readFileSync(fName) );
 const builds = readJson('polymer.json' ).builds || [];
-const deps = readJson('package-lock.json' ).dependencies || [];
-const mod2url = {};
+const deps = readJson('package-lock.json' ).dependencies || {};
+const pathMap = importmap.imports.filter( im=> (im.from+"").endsWith('\\//') )
+,    filesMap = importmap.imports.filter( im=>!(im.from+"").endsWith('\\//') );
+const path2im = {}
+,     path2mod = {};
+/**
+ * 1. traverse over build/, get list of files.
+ *      for each file:
+ *      - define package+version  from package-lock, keep undefined if not found
+ *      - assign target URL from importmap.js
+ * 2. traverse over build/, get list of files.
+ *      for each file:
+ *      - get import path
+ *      - resolve relative path to build/bundle/
+ *      - replace URL to pathMap[path].to expression
+ */
+
+glob.sync( `build/*/` ).map( buildFolder=>
+{
+    const buildName = buildFolder.split('/').pop();
+    glob.sync( `${buildFolder}/*` ).forEach( p=>process(p) );
+    function process( path, module )
+    {
+        if( isDirectory(path) )
+        {
+            if( !module && path.startsWith(`${buildFolder}node_modules/`)  )
+            {   let names = path.split('/');
+                let mPath = names.pop();
+                while( names.length )
+                {   if( module = deps[mPath] )
+                    {   module.name = mPath;
+                        break;
+                    }
+                    let n = names.pop();
+                    mPath = n+'/'+mPath;
+                }
+            }
+            glob.sync( `${path}/*` ).forEach( p => process( p, module ) );
+        }else
+        {   if( module )
+                path2mod[path] = module;
+            const im = filesMap.find( im => im.from.test ? im.from.test( path ) : im.from == path );
+            if ( !im )
+                return;
+            path2im[ path ] = im;
+        }
+    }
+});
+
+
+Object.keys(path2mod).map( p=>console.log(p, path2mod[p].name, path2mod[p].version ) )
+if(0)
+{
+
+
+
+
+
 
 builds.filter( b=>b.cdnRootUrl ).map( build =>
 {
@@ -105,4 +168,11 @@ function transform(code, id)
         code: magicString.toString(),
         map: magicString.generateMap()
     };
+}
+
+}
+
+function getModuleByPath( path )
+{
+    return
 }
---
+++ modules/polymer-portlet/src/main/resources/META-INF/resources/polymer3-demo/importmap.js
@@ -0,0 +1,43 @@
+/**
+ *    config to generate importmap.json and rewrite the JS imports from build/ folder into dist/ folder
+ */
+const importmap =
+{
+  imports:
+  /**
+   * the array traversed till find the first match of the path to "from" regEx or exact string
+   * ( path is relative to build/ folder )
+   *
+   * "from" is a JS regular expression or a string,
+   *    the actual values from name of module would be set in output to importmap.json
+   * "to" is a value to be set against key above within importmap.json
+   * keywords to be overridden:
+   *    ${buildName} - the build target name, a build/ subfolder
+   *    ${version} - module version from package-lock.json "dependencies" for current module( matching the folder )
+   *    ${packageName} - package name from package-lock.json or package.json "name" for current module
+   *    ${hash} - a hash for current file, has a sense as alternative for revision on module and buildName
+   * ? how to inject full url from baseUrl and is it really needed?
+   *
+   * "domain" is used as name of subfolder in dist/ and could be used for deployment
+   */
+    [   {   from:  /node_modules\/(\w+)\/(\w+)\//       // i.e. @polymer/iron-ajax/
+        ,   to : "/${buildName}/$1/$2@${version}/"      // i.e. /esm-unbundled/@polymer/app-layout@3.1.0
+        ,   domain: "cdn.xml4jquery.com"                // output files will reside in build/cdn.xml4jquery.com
+        }
+    ,   {   from:   /node_modules\/(\\w+)\//            // modules without org, like lodash/
+        ,   to:     "/${buildName}/$1@${version}/"      //    /esm-unbundled/lodash@4.17.15/
+        ,   domain: "cdn.xml4jquery.com"
+        }
+    ,   {  from:    /node_modules\/@foo\/(.*\.)([^\/]+)/    // @foo module files
+        ,   to :    "/${buildName}/@foo/$1.${hash}.$2"      // /esm-unbundled/@foo/main.3e6484ae5a646bd7c625 .js
+        ,   domain: "foo.io"
+        } // @foo does not use version and all its files are using hash instead
+    ,   {   from:  /(.+)/           // everything else. "default" is
+        ,   to : "/${buildName}/"   // i.e. /esm-unbundled/
+//      ,   to : "/${buildName}/{packageName}@{version}/"   // i.e. /esm-unbundled/@foo/polymer3-demo@0.1.0/
+        ,   domain: "default"       // is not really necessary default name for domain is a "default"
+        }
+    ]
+    // scopes: TBD
+};
+export default  importmap;
